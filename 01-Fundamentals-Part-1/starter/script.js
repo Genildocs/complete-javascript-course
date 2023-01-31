99392a2cc97f44f3653f20d2067f244230306bb2
Genildo <genildocs@gmail.com>
2023-01-31 05:59:55 -0300

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+/* let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is FUN');
+ */
+/* let primeiroNome = "Genildo";
+const PI = 3.1415;
+console.log(primeiroNome); */
+
+//LECTURE : Values and Variables
+
+/* let country = "Brasil";
+let continent = "South America";
+let population = "215 millions people"
+console.log(`Country: ${country}, continent: ${continent}, population: ${population}.`); */
