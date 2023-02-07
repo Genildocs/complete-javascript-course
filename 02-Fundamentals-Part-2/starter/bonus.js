47b2b83b52058e879e08d4534da45a31ea421b2a
genildocs <genildocs@gmail.com>
2023-02-07 02:59:44 -0300

    challenge1

---
+++ 02-Fundamentals-Part-2/starter/bonus.js
@@ -0,0 +1,33 @@
+//Desafio 3 de fundamentos parte 1
+
+let score = (score1,score2,score3)=> (score1 + score2 + score3) / 3;
+
+let averageScoreDophins = score(96,108,89);
+
+let averageScoreKoalas = score(88,91,110);
+
+function winner(time1, time2){
+    if(time1 >= 100 && time1 > time2){
+        console.log('Winner is Dophins!');
+    }else if(time2 >= 100 && time2 > time1){
+        console.log('Draw of the two teams!');
+    }else{
+        console.log('Winner is Koalas!');
+    }
+}
+
+winner(averageScoreDophins,averageScoreKoalas);
+
+console.log('----------')
+averageScoreDophins = score(97,112,101);
+
+averageScoreKoalas = score(109,95,123);
+
+winner(averageScoreDophins,averageScoreKoalas);
+
+console.log('----------')
+averageScoreDophins = score(97,112,101);
+
+averageScoreKoalas = score(109,95,106);
+
+winner(averageScoreDophins,averageScoreKoalas);
---
+++ 02-Fundamentals-Part-2/starter/challenge1.js
@@ -0,0 +1,31 @@
+
+
+let calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+
+
+let mediaGolfinhos = calcAverage(44,23,71);
+console.log(mediaGolfinhos);
+
+let mediaKoalas = calcAverage(65,54,49);
+console.log(mediaKoalas);
+
+function checWinners(mediaGolfinhos, mediaKoalas) {
+    if(mediaGolfinhos >= mediaKoalas * 2){
+        console.log(`Golfinhos ganhou! Pontuação: (${mediaGolfinhos} vs ${mediaKoalas})`)
+    }else if(mediaKoalas >= mediaGolfinhos * 2){
+        console.log(`Koalas ganhou! Pontuação: (${mediaKoalas} vs ${mediaGolfinhos})`)
+    }else{
+        console.log('Não houve vencedores!')
+    }
+}
+
+checWinners(mediaGolfinhos, mediaKoalas);
+console.log('--------------')
+mediaGolfinhos = calcAverage(85,54,71);
+
+mediaKoalas = calcAverage(23,34,27)
+
+checWinners(mediaGolfinhos, mediaKoalas);
+
+
