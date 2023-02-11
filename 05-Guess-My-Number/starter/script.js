1e7c85283045916a8a4aad5f8acfe1b4b4b91d66
genildocs <genildocs@gmail.com>
2023-02-11 04:13:06 -0300

    manipulation DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+console.log(document.querySelector('.message'). textContent);
+
+// document.querySelector('.message').textContent = 'Começar missão..';
+// document.querySelector('.label-score').textContent = 'Pontos:';
+// document.querySelector('.label-highscore').textContent = 'Numeros de pontos:';
+
+document.querySelector('.guess').value = 522;
+console.log(document.querySelector('.guess').value);
