714312808a224d11504c74993a641a7fdf944694
Genildo <genildocs@gmail.com>
2023-02-12 06:23:37 -0300

    implement logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,10 +1,30 @@
 'use strict';
 
-console.log(document.querySelector('.message'). textContent);
+/* console.log(document.querySelector('.message'). textContent);
 
 // document.querySelector('.message').textContent = 'Começar missão..';
 // document.querySelector('.label-score').textContent = 'Pontos:';
 // document.querySelector('.label-highscore').textContent = 'Numeros de pontos:';
 
 document.querySelector('.guess').value = 522;
-console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.guess').value); */
+
+
+const numberSecret = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = numberSecret; 
+
+document.querySelector('.check').addEventListener('click', function(){
+     const guess = Number(document.querySelector('.guess').value);
+     
+
+     if(!guess || guess < 0){
+          document.querySelector('.message').textContent = 'No number!';
+     }else if (guess === numberSecret){
+          document.querySelector('.message').textContent = '🎉🎉 Parabens! ACERTOU!!!';
+     }else if (guess > numberSecret){
+          document.querySelector('.message').textContent = '😕 Numero muito alto';
+     }else if (guess < numberSecret){
+          document.querySelector('.message').textContent = '😕 Numero muito baixo';
+     }
+})
+
