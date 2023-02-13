f0b2ef2106f80737db4161a5727153c1a9575f3c
genildocs <genildocs@gmail.com>
2023-02-12 23:01:34 -0300

    insert logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,15 +10,20 @@ document.querySelector('.guess').value = 522;
 console.log(document.querySelector('.guess').value); */
 
 
-const numberSecret = Math.trunc(Math.random() * 20) + 1;
+const numberSecret = Math.trunc(Math.random() * 20) + 1; // Gerando aleatoriamente um numero de 1 a 20
 document.querySelector('.number').textContent = numberSecret; 
 
+let score = 20; // Variavel com o numero de chances que o usuario tem para jogar
+
 document.querySelector('.check').addEventListener('click', function(){
-     const guess = Number(document.querySelector('.guess').value);
+     const guess = Number(document.querySelector('.guess').value); /* Estamos pegando o valor do input e armazenando na variavel guess
+     em seguida convertemos seu valor para Number */
+
      
 
-     if(!guess || guess < 0){
-          document.querySelector('.message').textContent = 'No number!';
+     if(!guess){ /* Essa condição verifica se tem algum valor no input de usuario, foi usado o operador NOT, pois se o valor for vazio
+     quando o usuario clicar será retornado false, o operador not vai inverter para true e esse bloco if será executado. */
+          document.querySelector('.message').textContent = 'No number!';          
      }else if (guess === numberSecret){
           document.querySelector('.message').textContent = '🎉🎉 Parabens! ACERTOU!!!';
      }else if (guess > numberSecret){
@@ -27,4 +32,3 @@ document.querySelector('.check').addEventListener('click', function(){
           document.querySelector('.message').textContent = '😕 Numero muito baixo';
      }
 })
-
