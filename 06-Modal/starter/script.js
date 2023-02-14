e6bb2b30eb0f52b9e8019b5eb41dd2e59a7ab60f
Genildo <genildocs@gmail.com>
2023-02-14 01:54:04 -0300

    SCRIPT

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const OpenModal = function () {
+  modal.classList.remove('hidden'); // Estamos removendo a classe hidden com esse metódo.
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden'); // Quando o botão for clicado a classe será adicionada novamente.
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', OpenModal); // Não invocamos a função pois o javaScript vai fazer isso automaticamente quando o botão for clicado.
+} //Loop na nodlist da classe show-modal
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal)
