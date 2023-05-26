e013b0b3124f0611972ed0128f065670db871ebf
genildocs <genildocs@gmail.com>
2023-05-26 00:22:02 -0300

    commit

--- 09-Data-Structures-Operators/starter/string.js
+++ 09-Data-Structures-Operators/starter/string.js
@@ -1,6 +1,23 @@
-const airline = 'Tap Air Portugal';
+/*const airline = 'Tap Air Portugal';
 const plane = 'A320';
 
 console.log(airline.indexOf('Tap'))
 console.log(airline.slice(4));
-console.log(airline);
+console.log(airline);*/
+
+const flights =
+    '_Delayed_Departure;fao93766109;txl2133758440;11:25' +
+    '+_Arrival;bru0943384722;fao93766109;11:45' +
+    '+_Delayed_Arrival;hel7439299980;fao93766109;12:05' +
+    '+_Departure;fao93766109;lis2323639855;12:30';
+
+const newFlights = flights.replaceAll('_',' ').split('+');
+console.log(newFlights);
+let arrayFlights = [];
+for (let i = 0; i < newFlights.length; i++){
+    arrayFlights = [i]
+}
+
+console.log(arrayFlights)
+
+
